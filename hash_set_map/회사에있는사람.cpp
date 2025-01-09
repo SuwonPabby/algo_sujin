fd616bfa814f06215870199de2a3b74028495dcd
lee-euiji <leeuj9663@naver.com>
2025-01-09 21:55:29 +0900

    250109_hash_set

---
+++ hash_set_map/회사에있는사람.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+
+#define REP(i, n) for (int i=0; i < n; i++)
+using namespace std;
+
+int main() {
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int n;
+  cin >> n;
+  unordered_set<string> names;
+  string name;
+  string cmd;
+  REP(i, n) {
+    cin >> name >> cmd;
+    if (cmd == "enter") {
+      names.insert(name);
+    } else {
+      names.erase(name);
+    }
+  }
+
+  vector<string> answer;
+  for (auto n: names){
+    answer.push_back(n);
+  }
+
+  sort(answer.begin(), answer.end(), greater<>());
+
+  for (auto n: answer) {
+    cout << n << "\n";
+  }
+}
