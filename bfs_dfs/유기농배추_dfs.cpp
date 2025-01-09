861a1a08373e02e1c44715c00d0be9625afb4740
lee-euiji <leeuj9663@naver.com>
2025-01-09 20:56:53 +0900

    250109_dfsbfs

---
+++ bfs_dfs/유기농배추_dfs.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <tuple>
+#define REP(i, n) for (int i=0; i < n; i++)
+using namespace std;
+
+int dr[4] = {1, -1, 0, 0};
+int dc[4] = {0, 0, 1, -1};
+
+void dfs(int r, int c, vector<vector<int>>& board, vector<vector<int>>& visited){
+  visited[r][c] = 1;
+
+  REP(i, 4) {
+    int nr = r + dr[i];
+    int nc = c + dc[i];
+
+    if (nr < 0 || nc < 0 || nr >= board.size() || nc >= board[0].size()) {
+      continue;
+    }
+
+    if (visited[nr][nc] == 1) continue;
+
+    if (board[nr][nc] == 0) continue;
+
+    dfs(nr, nc, board, visited);
+
+  }
+}
+
+int main(){
+  int T;
+  cin >> T;
+  REP(i, T) {
+    int M, N, K;
+    cin >> M >> N >> K;
+
+    vector<vector<int>> board(N, vector<int>(M, 0));
+    vector<vector<int>> visited(N, vector<int>(M, 0));
+
+    REP(i, K) {
+      int c, r;
+      cin >> c >> r;
+      board[r][c] = 1;
+    }
+
+    int answer = 0;
+    REP(row, N) {
+      REP(col, M) {
+        if (board[row][col] == 1 && visited[row][col] == 0) {
+          dfs(row, col, board, visited);
+          answer += 1;
+        }
+      }
+    }
+    cout << answer << endl;
+  }
+}
